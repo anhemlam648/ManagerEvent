e7978055076ac231ead61dca2aa02b7b2161138e
VuTrungNghia <vunghia467@gmail.com>
2024-07-29 21:10:33 +0700

    createService

---
+++ src/main/java/com/example/Manager/Service/EventService.java
@@ -0,0 +1,4 @@
+package com.example.Manager.Service;
+
+public interface EventService {
+}
---
+++ src/main/java/com/example/Manager/Service/Impl/LocationServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.Manager.Service.Impl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationServiceImpl {
+}
