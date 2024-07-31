e81ea8e4160f1c0957145652203d8338d8f311d4
VuTrungNghia <vunghia467@gmail.com>
2024-07-31 10:19:02 +0700

    updateMapperEvent

--- src/main/java/com/example/Manager/Mapper/MapEvent.java
+++ src/main/java/com/example/Manager/Mapper/MapEvent.java
@@ -2,24 +2,37 @@ package com.example.Manager.Mapper;
 
 import com.example.Manager.Dto.EventDto;
 import com.example.Manager.Entity.Event;
+import com.example.Manager.Entity.Location;
+import com.example.Manager.Repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MapEvent {
-    public static EventDto MapDtoEvent(Event event){
+    @Autowired
+    private LocationRepository locationRepository;
+
+    public EventDto MapDtoEvent(Event event) {
         EventDto eventDto = new EventDto();
         eventDto.setId(event.getId());
         eventDto.setName(event.getName());
         eventDto.setDescription(event.getDescription());
         eventDto.setStartDate(event.getStartDate());
         eventDto.setEndDate(event.getEndDate());
+        eventDto.setLocationId(event.getLocation().getId());
         return eventDto;
     }
-    public static Event MapEvent(EventDto eventDto){
+
+    public Event MapEvent(EventDto eventDto) {
         Event event = new Event();
         event.setId(eventDto.getId());
         event.setName(eventDto.getName());
         event.setDescription(eventDto.getDescription());
         event.setStartDate(eventDto.getStartDate());
         event.setEndDate(eventDto.getEndDate());
+        Location location = locationRepository.findById(eventDto.getLocationId())
+                .orElseThrow(() -> new RuntimeException("Location not found"));
+        event.setLocation(location);
         return event;
     }
 }
