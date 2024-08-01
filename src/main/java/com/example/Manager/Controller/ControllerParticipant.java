e01c171aff7c4c43268e4c17cfb75a79b3e05c73
VuTrungNghia <vunghia467@gmail.com>
2024-08-01 12:15:19 +0700

    updateControllerParticipant

--- src/main/java/com/example/Manager/Controller/ControllerParticipant.java
+++ src/main/java/com/example/Manager/Controller/ControllerParticipant.java
@@ -1,14 +1,49 @@
 package com.example.Manager.Controller;
 
+import com.example.Manager.Dto.ParticipantDto;
+import com.example.Manager.Service.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/participants")
 @CrossOrigin("http://localhost:3030")
 public class ControllerParticipant {
+    @Autowired
+    private ParticipantService participantService;
 
+    @PostMapping
+    public ResponseEntity<ParticipantDto> createParticipant(@RequestBody ParticipantDto participantDto) {
+        ParticipantDto createdParticipant = participantService.createParticipant(participantDto);
+        return ResponseEntity.ok(createdParticipant);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ParticipantDto> updateParticipant(@PathVariable Long id, @RequestBody ParticipantDto participantDto) {
+        ParticipantDto updatedParticipant = participantService.updateParticipant(id, participantDto);
+        return ResponseEntity.ok(updatedParticipant);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteParticipant(@PathVariable Long id) {
+        participantService.deleteParticipant(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ParticipantDto> getParticipantById(@PathVariable Long id) {
+        ParticipantDto participantDto = participantService.getParticipantById(id);
+        return ResponseEntity.ok(participantDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ParticipantDto>> getAllParticipants() {
+        List<ParticipantDto> participants = participantService.getAllParticipants();
+        return ResponseEntity.ok(participants);
+    }
 }
